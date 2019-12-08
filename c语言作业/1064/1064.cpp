e01a5d2c0a63435fe4058c64fe66f6d1f40f3885
Irilia <1473663035@qq.com>
2019-12-08 19:50:44 +0800

    1064

---
+++ c语言作业/1064/1064.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+int main() {
+	double balance;
+	double sum = 0;
+	for (int i = 0; i < 12; ++i) {
+		scanf("%lf", &balance);
+		sum += balance;
+	}
+	printf("$%.2f\n", sum / 12.0);
+	return 0;
+}
